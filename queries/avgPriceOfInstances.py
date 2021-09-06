3aeeffb54c6092f36b6fb76f467e16588ca3f914
adrnliaw <adrnliaw@gmail.com>
2021-09-06 13:09:30 +0800

    updated comments into query

--- queries/avgPriceOfInstances.py
+++ queries/avgPriceOfInstances.py
@@ -18,28 +18,44 @@ df1 = pd.DataFrame([])
 # input variable
 os = "Windows"
 
+# loop to append all collections into one dataFrame
 for region in REGIONS:
     mycol = mydb[region]
+
+    # concatenate region name and retrieve os and price columns from mongodb
     mydoc = mycol.aggregate([ 
         {"$match": {"os": os } },
-        {"$project": { "region": { "$concat": ["$region.endpoint", "-", "$region.zone"] },
-         "os": "$os", "price" : "$price"}} ])
+        {"$project": { "region": { "$concat": ["$region.endpoint", "-", "$region.zone"] }, "price" : "$price"}} ])
     
+    # # convert data retrieved to dataFrame
     df1 = pd.DataFrame(list(mydoc))
 
+    # append data retrieved into the master dataFrame
     df = df.append(df1)
 
-df = df[["region", "price"]]
-
+# Group the data by region
 df = df.groupby("region").mean().reset_index()
 
+# rename the column name
 df = df.rename(columns={"region": "Region", "price": "Average Price"})
 
-
+# set size for graph
 fig = plt.figure(figsize = (10, 5))
+
+# plot graph
 plt.bar(df["Region"], df["Average Price"], width = 0.4)
+
+# show x axis in vertical
 plt.xticks(rotation=90)
+
+# naming the x axis
 plt.xlabel("Region")
+
+# naming the y axis
 plt.ylabel("Average Price")
+
+# title of graph
 plt.title("Average price of Instances using " + os + " Operating System Across Different Regions")
+
+# show graph
 plt.show()
--- queries/frequencyOfInstances.py
+++ queries/frequencyOfInstances.py
@@ -11,19 +11,32 @@ from globals import REGIONS
 db = pymongo.MongoClient("mongodb://localhost:27017/").aws_spot
 # input variable is the region (collections)
 region = REGIONS[3]
+
+# query only the instance family column from mongodb
 docs = db[region].aggregate([
     {"$project": { "Instance": { "$concat": ["$instance_type.family"] }}}
 ])
-#docs = db[region].find({}, {"instance_type.family": 1, "os": 1})
 
+# convert data retrieved to dataFrame
 df = pd.DataFrame(list(docs))
 
+# Get frequency of instance
 df = df["Instance"].value_counts().rename_axis("Instance").reset_index(name="Frequency")
 
+# set size for graph
 fig = plt.figure(figsize = (10, 5))
+
+# plot graph
 plt.bar(df["Instance"], df["Frequency"], width = 0.4)
  
+# naming the x axis
 plt.xlabel("Instance")
+
+# naming the y axis
 plt.ylabel("Frequency")
+
+# title of graph
 plt.title("Instance Frequency in " + region)
+
+# show graph
 plt.show()
