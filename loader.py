d55381138db14bb4093cc03f93b1a5beafffa62e
Nightr0ck <leequan2000@gmail.com>
2021-09-06 16:06:38 +0800

    delete legacy loaders

--- loader.py
+++ loader.py
@@ -1,41 +1,43 @@
 import pymongo
 import pandas as pd
 from globals import REGIONS
+from datetime import datetime
 
 db = pymongo.MongoClient("mongodb://localhost:27017/")
-db.drop_database("aws_spot_by_region")
-db = db.aws_spot_by_region
+db.drop_database("aws_spot")
+db = db.aws_spot
+
+def family_size(instance_type):
+    family, size = instance_type.split(".")
+    return {"family": family, "size": size}
+
+def endpoint_zone(region):
+    end1, end2, zone = region.split("-")
+    return {"endpoint": end1 + "-" + end2, "zone": zone}
+
+def datetime_object(timestamp):
+    date, time = timestamp.split(" ")
+    year, month, day = date.split("-")
+    hour, minute, second = time.split(":")[:3]
+    second = second.split("+")[0]
+
+    return datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))
 
 for region in REGIONS:
     df = pd.read_csv("./data/" + region.replace("_", "-") + ".csv", header=None)
     df.rename(columns={0:"timestamp", 1:"instance_type", 2:"os", 3:"region", 4:"price"}, inplace=True)
+    df["timestamp"] = df["timestamp"].apply(datetime_object)
+    df["instance_type"] = df["instance_type"].apply(family_size)
+    df["region"] = df["region"].apply(endpoint_zone)
+    print(df.head(5))
+    print("converting dataframe into dictionary")
+    data = df.to_dict(orient="records")
+    print("inserting into " + region)
+    db[region].insert_many(data)
+    print("inserted")
 
-    family_size_dict = {}
-    for instance_type in df["instance_type"].unique():
-        family, size = instance_type.split(".")
-        db[region].insert_one({
-            "family": family,
-            "size": size,
-            "spot_instance": []
-        })
-    
-    family_size_index = pymongo.IndexModel([("family", pymongo.ASCENDING), ("size", pymongo.ASCENDING)])
-    db[region].create_indexes([family_size_index])
-    
-
-    for index, row in df.iterrows():
-        date, time = row["timestamp"].split(" ")
-        family, size = row["instance_type"].split(".")
-        os = row["os"]
-        availability_zone = row["region"].split("-")[2]
-        price = row["price"]
-
-        print(index)
-
-        db[region].update_one(
-            {"family": family, "size": size},
-            {"$push": {"spot_instance": {"date": date, "time": time, "os": os, "availability_zone": availability_zone, "price": price}}}
-        )
-
-
-
+    index = pymongo.IndexModel([("instance_type.family", pymongo.ASCENDING), ("instance_type.size", pymongo.ASCENDING), ("os", pymongo.ASCENDING), ("timestamp", pymongo.DESCENDING)])
+    print("creating indexes")
+    db[region].create_indexes([index])
+    print("indexed")
+#{"datetime": datetime object, instance_type:{{"family": family}, {"size", size}}, "os": os, "region":{{"endpoint": endpoint}, {"zone": zone}}, "price": price}
