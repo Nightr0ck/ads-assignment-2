2b107900759e07b41802605366f7309fbd235e50
adrnliaw <adrnliaw@gmail.com>
2021-09-06 12:40:53 +0800

    update query

--- queries/frequencyOfInstances.py
+++ queries/frequencyOfInstances.py
@@ -12,19 +12,18 @@ db = pymongo.MongoClient("mongodb://localhost:27017/").aws_spot
 # input variable is the region (collections)
 region = REGIONS[3]
 docs = db[region].aggregate([
-        {"$project": { "Instance": { "$concat": ["$instance_type.family", "-", "$instance_type.size"] },
-         "OS": "$os"}} ])
+    {"$project": { "Instance": { "$concat": ["$instance_type.family"] }}}
+])
+#docs = db[region].find({}, {"instance_type.family": 1, "os": 1})
 
 df = pd.DataFrame(list(docs))
 
 df = df["Instance"].value_counts().rename_axis("Instance").reset_index(name="Frequency")
 
-print(df)
-
-#fig = plt.figure(figsize = (10, 5))
-#plt.bar(df["Instance"], df["Frequency"], width = 0.4)
+fig = plt.figure(figsize = (10, 5))
+plt.bar(df["Instance"], df["Frequency"], width = 0.4)
  
-#plt.xlabel("OS")
-#plt.ylabel("Number of Instances")
-#plt.title("Instance OS distribution in " + region)
-#plt.show()
+plt.xlabel("Instance")
+plt.ylabel("Frequency")
+plt.title("Instance Frequency in " + region)
+plt.show()
